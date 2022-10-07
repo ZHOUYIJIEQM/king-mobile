43ff9a996f6fbf4d2c40aaafd7029465db6384d7
ZHOUYIJIEQM <husiluanxianguser@163.com>
2022-10-07 15:35:15 +0800

    修改请求地址

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
+import './utils/amfeFlexible.js';
 import request from "./request/index";
 import toast from "./components/Toast/index";
 import "./styles/global.scss";
